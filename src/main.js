9566a9fc0626bd40a9868822a490d78cb07f95aa
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-11-16 10:23:21 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ const mongo = require("./mongo");
 async function expressGet() {
     await mongo.mongoConnect();
 
+    /*In mongoDB our NFTs are sorted by chain therefore the first parameter is chain.
+      The second parameter for the endpoint is ContractAddress to return all instances of that address*/
     app.get('/contract/:chain/:address', async (req, res) => {
+        //assign params to variables
         var query = req.params.address;
         var chain = req.params.chain
+        //look for data in mongoDB
         var data = await mongo.mongoRead(chain, { ContractAddress: query });
         console.log(data);
+        //send our response
         res.send(data);
     })
 }
 expressGet();
 
 app.listen(3002, () => console.log('Listening on port 3002'));
-//0xf102F6d0759B0cf6B5B61745ec51fc20F7F67A21
